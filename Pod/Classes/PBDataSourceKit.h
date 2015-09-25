f09a34a5c204b296286331336664be58b2853c31
Philippe Blondin <info@philippeblondin.ca>
2015-09-25 11:47:20 -0400

    Add header file

---
+++ Pod/Classes/PBDataSourceKit.h
@@ -0,0 +1,19 @@
+//
+//  PBDataSourceKit.h
+//  Pods
+//
+//  Created by Philippe Blondin on 2015-09-25.
+//
+//
+
+#ifndef PBDataSourceKit_h
+#define PBDataSourceKit_h
+
+#import "PBDataProvider.h"
+#import "PBDataReceiver.h"
+#import "PBDataCellConfigure.h"
+#import "PBTableDataViewController.h"
+#import "PBCollectionDataViewController.h"
+#import "PBDataLoadingOperation.h"
+
+#endif /* PBDataSourceKit_h */
